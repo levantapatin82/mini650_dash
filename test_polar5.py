ea66d35bbf22e91893ed8118d02c5817c11f48ad
levantapatin82 <levantapatin@gmail.com>
2025-12-19 12:57:06 +0100

    code update

--- test_polar5.py
+++ test_polar5.py
@@ -220,8 +220,12 @@ with tab1:
 
         df_avg_dict = {bt: smooth_for_polar(dfin[dfin["boat type"]==bt], smooth=smooth1) for bt in boat_types}
 
+        #colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+        #          "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
         colors = ["#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-                  "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf"]
+                    "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+                    "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5",
+                    "#c49c94", "#f7b6d2", "#dbdb8d", "#9edae5", "#393b79"]
         color_map = {bt: colors[i % len(colors)] for i, bt in enumerate(boat_types)}
 
         fig = sp.make_subplots(
